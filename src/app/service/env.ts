f60e22baef1e55aceab84e647ed455da550bf8a3
emmanuel ephrim <emmaephrim@gmail.com>
2024-10-04 05:58:53 +0000

    tring bundle issues fixes

--- src/app/service/env.ts
+++ src/app/service/env.ts
@@ -1,2 +1,2 @@
-export const apiBaseUrl = 'http://localhost:8080';
-// export const apiBaseUrl = 'https://blog-app-v1-tagname.onrender.com';
+// export const apiBaseUrl = 'http://localhost:8080';
+export const apiBaseUrl = 'https://blog-app-v1-tagname.onrender.com';
